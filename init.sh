6e859d573b7cb8ef1ec5811a62bcf122a2320825
Michael <managedkaos@users.noreply.github.com>
2023-02-13 23:08:53 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
+export TF_SWITCH_VERSION=0.13.1308
+export PATH=${PATH}:${HOME}/.local/bin
 mkdir -p ~/.local/bin
-wget https://github.com/warrensbox/terraform-switcher/releases/download/0.13.1308/terraform-switcher_0.13.1308_linux_amd64.tar.gz -O /tmp/terraform-switcher_0.13.1308_linux_amd64.tar.gz
-tar -xzvf /tmp/terraform-switcher_0.13.1308_linux_amd64.tar.gz --directory=$HOME/.local/bin tfswitch
+wget https://github.com/warrensbox/terraform-switcher/releases/download/${TF_SWITCH_VERSION}/terraform-switcher_${TF_SWITCH_VERSION}_linux_amd64.tar.gz -O /tmp/tfswitch.tar.gz
+tar -xzvf /tmp/tfswitch.tar.gz --directory=${HOME}/.local/bin tfswitch
 tfswitch --latest
-alias tf=terraform
 
+alias tf=terraform
+alias init='terraform init'
+alias validate='terraform validate'
+alias plan='terraform plan'
+alias apply='terraform apply'
+alias approve='terraform apply -auto-approve'
+alias destroy='terraform apply -destroy'
