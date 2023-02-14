b290a041c8c0ea1c91e0b062d208c0acb8edcb44
Michael <managedkaos@users.noreply.github.com>
2023-02-13 21:06:32 -0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.local/bin
+wget https://github.com/warrensbox/terraform-switcher/releases/download/0.13.1308/terraform-switcher_0.13.1308_linux_amd64.tar.gz -O /tmp/terraform-switcher_0.13.1308_linux_amd64.tar.gz
+tar -xzvf /tmp/terraform-switcher_0.13.1308_linux_amd64.tar.gz --directory=$HOME/.local/bin tfswitch
