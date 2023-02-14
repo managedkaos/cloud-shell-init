fed31844472fcfe7ad69b4da322f9f00c8aa1d58
Michael <managedkaos@users.noreply.github.com>
2023-02-13 23:10:24 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.local/bin
 wget https://github.com/warrensbox/terraform-switcher/releases/download/${TF_SWITCH_VERSION}/terraform-switcher_${TF_SWITCH_VERSION}_linux_amd64.tar.gz -O /tmp/tfswitch.tar.gz
 tar -xzvf /tmp/tfswitch.tar.gz --directory=${HOME}/.local/bin tfswitch
 tfswitch --latest
+terraform --version || echo "Perhaps terraform was not installed correctly :("
 
 alias tf=terraform
 alias init='terraform init'
@@ -11,4 +12,5 @@ alias validate='terraform validate'
 alias plan='terraform plan'
 alias apply='terraform apply'
 alias approve='terraform apply -auto-approve'
+alias output='terraform output'
 alias destroy='terraform apply -destroy'
