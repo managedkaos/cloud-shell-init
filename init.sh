69d739ee937c1679568bfdbff466312e87f93d40
Michael <managedkaos@users.noreply.github.com>
2023-02-21 10:45:47 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 export TF_SWITCH_VERSION=0.13.1308
 export PATH=${PATH}:${HOME}/.local/bin
-mkdir -p ~/.local/bin
+mkdir -p ${HOME}/.local/bin
 wget https://github.com/warrensbox/terraform-switcher/releases/download/${TF_SWITCH_VERSION}/terraform-switcher_${TF_SWITCH_VERSION}_linux_amd64.tar.gz -O /tmp/tfswitch.tar.gz
 tar -xzvf /tmp/tfswitch.tar.gz --directory=${HOME}/.local/bin tfswitch
-tfswitch --latest
+
+tfswitch --latest || echo "Perhaps tfswitch was not installed correctly :(" 
 terraform --version || echo "Perhaps terraform was not installed correctly :("
 
 alias tf=terraform
